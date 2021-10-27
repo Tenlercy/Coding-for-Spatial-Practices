d7be0c42af7ae36efb5769bbc08afc26fb89c10f
Tenlercy <83601451+Tenlercy@users.noreply.github.com>
2021-10-27 11:16:07 -0400

    Update sript.js

--- intro-to-javascript/sript.js
+++ intro-to-javascript/sript.js
@@ -1,2 +1,7 @@
-console. log("Hello World");
+let noun = "dog";
+let verb = "catched";
+let adjective = "cuddly";
 
+let sentence = 'My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky.';
+
+console.log(sentence);
