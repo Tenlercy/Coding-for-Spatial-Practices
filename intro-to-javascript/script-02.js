4403c82baf199b099953edbd8f78b381c68abe16
Tenlercy <83601451+Tenlercy@users.noreply.github.com>
2021-11-16 19:20:02 -0500

    Update script-02.js

---
+++ intro-to-javascript/script-02.js
@@ -0,0 +1,10 @@
+let age = 68;
+let height = 5;
+
+if(age < 8){
+    console.log("check out the Merry-Go-Round. You'll love it");
+} else if (age > 8 && age < 65 && height > 4.5) {
+    console.log("check out the Roller Coaster. It's awesome!");
+} else {
+    console.log("Why not enjoy a float down the Lazy River?");
+}
