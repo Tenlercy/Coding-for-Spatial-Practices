4dd70406048e872ea06983d8058414634f8a057b
Tenlercy <83601451+Tenlercy@users.noreply.github.com>
2021-12-20 23:31:14 -0500

    1

--- Exercise 07/script.js
+++ Exercise 07/script.js
@@ -1,25 +1,24 @@
-console.log('it works')
+var verbs, nouns, adjectives, adverbs, preposition;
+nouns = ["sun", "moon", "people", "village", "town", "soilder", "civilian"];
+verbs = ["cry", "break", "build", "cough", "dance", "clapped", "amused"];
+adjectives = ["brave", "busy", "cautious", "charming", "careful", "breakable", "clever"];
+adverbs = ["devotedly", "elegantly", "gracefully", "merrily", "safely", "vivaciously", "innocently"];
+preposition = ["down", "into", "up", "on", "upon", "below", "above"];
 
-let nouns = ["heart","rainbow","ocean"];
-let verbs = ["look","make","continue"];
-let adjectives = ["good", "different","possible"];
+function randGen() {
+  return Math.floor(Math.random() * 5);
+}
 
-// let item = arrayName[Math.floor(Math.random()*arrayName.length)];
+function sentence() {
+  var rand1 = Math.floor(Math.random() * 7);
+  var rand2 = Math.floor(Math.random() * 7);
+  var rand3 = Math.floor(Math.random() * 7);
+  var rand4 = Math.floor(Math.random() * 7);
+  var rand5 = Math.floor(Math.random() * 7);
+  var rand6 = Math.floor(Math.random() * 7);
 
-// let noun = nouns[Math.floor(Math.random()*arrayName.length)]
+  var content = "The " + adjectives[rand1] + " " + nouns[rand2] + " " + adverbs[rand3] + " " + verbs[rand4] + " because some " + nouns[rand1] + " " + adverbs[rand1] + " " + verbs[rand1] + " " + preposition[rand1] + " a " + adjectives[rand2] + " " + nouns[rand5] + " which, became a " + adjectives[rand3] + ", " + adjectives[rand4] + " " + nouns[rand6] + ".";
 
-// let random = Math.floor(Math.random()*arrayName.length)
-let noun = nouns[Math.floor(Math.random()*nouns.length)]
-let verb = verbs[Math.floor(Math.random()*verbs.length)]
-let adjective = adjectives[Math.floor(Math.random()*adjectives.length)]
-
-// console.log(noun)
-// console.log(verb)
-// console.log(adjective)
-
-let sentence = 'My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky.';
-
-console.log(sentence);
-
-// console.log(Math.floor(Math.random()*arrayName.length))
-
+  document.getElementById('sentence').innerHTML = "&quot;" + content + "&quot;";
+};
+sentence();
